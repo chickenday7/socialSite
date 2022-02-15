30456a0b89d73adc97f438d646fcbe34adf301a4
chickenday7 <chickenday777@gmail.com>
2022-02-15 07:36:10 -0800

    add styled for login page

--- src/components/Content/Login/LoginForm/LoginForm.tsx
+++ src/components/Content/Login/LoginForm/LoginForm.tsx
@@ -4,6 +4,7 @@ import {SubmitHandler, useForm} from "react-hook-form";
 import {LoginDataRequestType} from "../../Redux/authReducer";
 import {Navigate} from "react-router-dom";
 import Preloader from "../../Preloader/Preloader";
+import {TextField} from "@material-ui/core";
 
 type Inputs = {
     email: string
@@ -16,9 +17,9 @@ type LoginFormPropsType = {
     login: (loginData: LoginDataRequestType) => void
     isFetching: boolean
     isAuth: boolean
-    id:number | null
-    toggleError:(stateError:boolean)=>void
-    authError:boolean
+    id: number | null
+    toggleError: (stateError: boolean) => void
+    authError: boolean
 }
 
 export const LoginForm = (props: LoginFormPropsType) => {
@@ -26,7 +27,7 @@ export const LoginForm = (props: LoginFormPropsType) => {
 
 
     const checkChanged = () => {
-      props.toggleError(false)
+        props.toggleError(false)
     }
 
     const onSubmit: SubmitHandler<Inputs> = (data) => {
@@ -36,26 +37,33 @@ export const LoginForm = (props: LoginFormPropsType) => {
         return <Navigate to={`/profile/${props.id}`}/>
     }
     return (
-        <>
+        <div className={s.formContainer}>
             {props.isFetching
-                ? <Preloader />
+                ? <Preloader/>
                 : <form className={s.formWrapper} onSubmit={handleSubmit(onSubmit)}>
                     <label>Login</label>
                     <input onClick={checkChanged} placeholder={'Login'} className={s.input}
                            defaultValue={''} {...register('email', {required: true})}/>
                     {errors.email && <span>This field is required</span>}
                     <label>Password</label>
-                    <input onClick={checkChanged} className={s.input} placeholder={'Password'} type={'password'} {...register('password', {required: true})} />
+                    <TextField label="Password"
+                               type="password"
+                               autoComplete="current-password"
+                               variant="standard"
+                               onClick={checkChanged}
+                               className={s.input}
+                               {...register('password', {required: true})}
+                    />
                     {errors.password && <span>This field is required</span>}
                     <div className={s.checkBox}>
                         <input type={'checkbox'} {...register('rememberMe')} />
                         <span>remember me</span>
                     </div>
-                    {props.authError && <span style={{color:'red'}}>Email or password is wrong</span>}
+                    {props.authError && <span style={{color: 'red'}}>Email or password is wrong</span>}
                     <input type={'submit'}/>
                 </form>
             }
 
-        </>
+        </div>
     )
 }
